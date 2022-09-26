3e85e30fa7e4eafa740500d1298b41e3890c9e15
Aji Ninan <ajininan72@gmail.com>
2022-09-26 10:18:14 +0530

    edit product

--- helpers/product-helpers.js
+++ helpers/product-helpers.js
@@ -22,6 +22,26 @@ module.exports = {
                 resolve(response);
             });
         });
+    },
+    getProductDetails: (prodId) => {
+        return new Promise ((resolve, reject) => {
+            db.get().collection(collection.PRODUCT_COLLECTION).findOne({_id:objectId(prodId)}).then((product) => {
+                resolve(product);
+            })
+        })
+    },
+    updateProduct: (prodId, proDetails) => {
+        return new Promise ((resolve, reject) => {
+            db.get().collection(collection.PRODUCT_COLLECTION).updateOne({_id:objectId(prodId)}, {$set:{
+                Name: proDetails.Name,
+                Description: proDetails.Description,
+                Price: proDetails.Price,
+                Category: proDetails.Category
+            }
+            }).then((response)=> {
+                resolve()
+            })
+        })
     }
 }
 // everything is going to be exported
