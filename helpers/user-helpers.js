9904cb5a22c1f40f47517704a53558bf26c57afe
Aji Ninan <ajininan72@gmail.com>
2022-09-28 18:15:37 +0530

    product counter added

--- helpers/user-helpers.js
+++ helpers/user-helpers.js
@@ -1,38 +1,157 @@
-var db = require('../config/connection');
-var collection = require('../config/collections');
-const bcrypt = require('bcrypt');
+var db = require("../config/connection");
+var collection = require("../config/collections");
+const bcrypt = require("bcrypt");
+var objectId = require("mongodb").ObjectID;
 module.exports = {
-    doSignup: (userData) => {
-        return new Promise(async(resolve, reject) => {
-            userData.Password = await bcrypt.hash(userData.Password, 10);
-            db.get().collection(collection.USER_COLLECTION).insertOne(userData).then((data) => {
-                resolve(data.insertedId);
+  doSignup: (userData) => {
+    return new Promise(async (resolve, reject) => {
+      userData.Password = await bcrypt.hash(userData.Password, 10);
+      db.get()
+        .collection(collection.USER_COLLECTION)
+        .insertOne(userData)
+        .then((data) => {
+          console.log(data);
+          resolve(data.insertedId);
+        });
+    });
+  },
+  doLogin: (userData) => {
+    let loginStatus = false;
+    let response = {};
+    return new Promise(async (resolve, reject) => {
+      let user = await db
+        .get()
+        .collection(collection.USER_COLLECTION)
+        .findOne({ Email: userData.Email });
+      if (user) {
+        bcrypt.compare(userData.Password, user.Password).then((status) => {
+          // this will automatically compare we don't have to do it manually
+          if (status) {
+            console.log("login success");
+            response.user = user;
+            response.status = true;
+            resolve(response);
+          } else {
+            console.log("login failed");
+            resolve({ status: false });
+          }
+        });
+      } else {
+        console.log("Email not registered");
+        resolve({ status: false });
+      }
+    });
+  },
+  addToCart: (proId, userId) => {
+    let proObj = {
+      item: objectId(proId),
+      quantity: 1,
+    };
+    return new Promise(async (resolve, reject) => {
+      let userCart = await db
+        .get()
+        .collection(collection.CART_COLLECTION)
+        .findOne({ user: objectId(userId) });
+      if (userCart) {
+        let proExist = userCart.products.findIndex(product => product.item == proId);
+        console.log(proExist);
+        if (proExist != -1) {
+          db.get()
+            .collection(collection.CART_COLLECTION)
+            .updateOne(
+              { 'products.item': objectId(proId) },
+              {
+                $inc: { 'products.$.quantity': 1 },
+              }
+            ).then(() => {
+                resolve()
             })
-        })
-
-    },
-    doLogin: (userData) => {
-        let loginStatus = false;
-        let response = {};
-        return new Promise(async (resolve, reject) => {
-            let user = await db.get().collection(collection.USER_COLLECTION).findOne({Email:userData.Email})
-            if(user){
-                bcrypt.compare(userData.Password, user.Password).then((status) => {
-                    // this will automatically compare we don't have to do it manually
-                    if(status){
-                        console.log("login success");
-                        response.user = user;
-                        response.status = true;
-                        resolve(response);
-                    }else{
-                        console.log("login failed");
-                        resolve({ status:false });
-                    }
-                })
-            }else{
-                console.log("Email not registered");
-                resolve({ status:false });
+        } else {
+          db.get()
+            .collection(collection.CART_COLLECTION)
+            .updateOne(
+              { user: objectId(userId) },
+              {
+                $push: { products: proObj },
+              }
+            )
+            .then((response) => {
+              resolve();
+            });
+        }
+      } else {
+        let cartObj = {
+          user: objectId(userId),
+          products: [proObj],
+        };
+        db.get()
+          .collection(collection.CART_COLLECTION)
+          .insertOne(cartObj)
+          .then((response) => {
+            resolve();
+          });
+      }
+    });
+  },
+  getCartProducts: (userId) => {
+    return new Promise(async (resolve, reject) => {
+      let cartItems = await db
+        .get()
+        .collection(collection.CART_COLLECTION)
+        .aggregate([
+          {
+            $match: {user:objectId(userId)},
+          },
+          {
+            $unwind:'$products'
+          },
+          {
+            $project:{
+                item: '$products.item',
+                quantity: '$products.quantity'
             }
-        })
-    } 
-}
+          },
+          {
+            $lookup:{
+                from:collection.PRODUCT_COLLECTION,
+                localField:'item',
+                foreignField:'_id',
+                as:'product'
+            }
+          }
+        //   {
+        //     $lookup: {
+        //       from: collection.PRODUCT_COLLECTION,
+        //       let: { prodList: "$products" },
+        //       pipeline: [
+        //         {
+        //           $match: {
+        //             $expr: {
+        //               $in: ["$_id", "$$prodList"],
+        //             },
+        //           },
+        //         },
+        //       ],
+        //       as: "cartItems",
+        //     },
+        //   },
+        ])
+        .toArray()
+        console.log(cartItems[0].products);
+      resolve(cartItems);
+    });
+  },
+  getCartCount: (userId) => {
+    return new Promise(async (resolve, reject) => {
+      let count = 0;
+      let cart = await db
+        .get()
+        .collection(collection.CART_COLLECTION)
+        .findOne({ user: objectId(userId) });
+      if (cart) {
+        count = cart.products.length;
+      }
+      resolve(count);
+    });
+  },
+};
