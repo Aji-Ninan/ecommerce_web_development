c33afa375ab970bb089b5ff3bcdeaad0207efe95
Aji Ninan <ajininan72@gmail.com>
2022-10-01 15:35:05 +0530

    checkout page and total amount

--- routes/user.js
+++ routes/user.js
@@ -69,8 +69,9 @@ router.get("/logout", (req, res) => {
 });
 router.get("/cart", verifyLogin, async (req, res) => {
   let products = await userHelpers.getCartProducts(req.session.user._id);
+  let totalValue = await userHelpers.getTotalAmount(req.session.user._id);
   console.log(products);
-  res.render("user/cart", { products, user: req.session.user });
+  res.render("user/cart", { products, user: req.session.user, totalValue });
 });
 router.get("/add-to-cart/:id", (req, res) => {
   console.log("api call");
@@ -79,8 +80,9 @@ router.get("/add-to-cart/:id", (req, res) => {
     res.json({status:true})
   });
 });
-router.post('/change-product-quantity', (req, res, next) => {
-  userHelpers.changeProductQuantity(req.body).then((response) =>{
+router.post('/change-product-quantity/', (req, res, next) => {
+  userHelpers.changeProductQuantity(req.body).then(async (response) =>{
+    response.total = await userHelpers.getTotalAmount(req.body.user);
     res.json(response)
   })
 })
@@ -89,5 +91,8 @@ router.post('/remove-product', (req, res, next) => {
     res.json(response)
   })
 })
-
+router.get('/place-order',verifyLogin, async (req, res) => {
+  let total = await userHelpers.getTotalAmount(req.session.user._id)
+  res.render('user/place-order', {total})
+})
 module.exports = router;
