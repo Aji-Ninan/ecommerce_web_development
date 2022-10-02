2c161259c35c0f14a175d6fbe2b22de19b8a9aba
Aji Ninan <ajininan72@gmail.com>
2022-10-02 16:45:45 +0530

    Payment -> COD

--- helpers/user-helpers.js
+++ helpers/user-helpers.js
@@ -1,6 +1,8 @@
 var db = require("../config/connection");
 var collection = require("../config/collections");
 const bcrypt = require("bcrypt");
+const { response } = require("express");
+const { ObjectId } = require("mongodb");
 var objectId = require("mongodb").ObjectID;
 module.exports = {
   doSignup: (userData) => {
@@ -258,5 +260,80 @@ module.exports = {
       console.log(total[0].total);
       resolve(total[0].total);
     });
+  },
+  placeOrder : (order, products, total) => {
+    return new Promise((resolve, reject) => {
+      console.log(order, products, total);
+      let status = order['payment-method'] === 'COD' ? 'placed' : 'pending';
+      let orderObj = {
+        deliveryDetails:{
+          mobile: order.mobile,
+          address: order.address,
+          pincode: order.pincode
+        },
+        userId: objectId(order.userId),
+        paymentMethod: order['payment-method'],
+        products: products,
+        totalAmount: total,
+        status: status,
+        date: new Date()
+      }
+
+      db.get().collection(collection.ORDER_COLLECTION).insertOne(orderObj).then((response) => {
+        db.get().collection(collection.CART_COLLECTION).deleteOne({user: ObjectId(order.userId)})
+        resolve()
+      })
+
+    })
+  },
+  getCartProductList: (userId) => {
+    return new Promise(async (resolve, reject) => {
+      let cart = await db.get().collection(collection.CART_COLLECTION).findOne({user:objectId(userId)})
+      resolve(cart.products)
+    })
+  },
+  getUserOrders : (userId) => {
+    return new Promise( async (resolve, reject) => {
+      let orders = await db.get().collection(collection.ORDER_COLLECTION).find({userId: objectId(userId)}).toArray()
+      resolve(orders)
+    })
+  },
+  getOrderProducts: (orderId) => {
+    return new Promise(async (resolve, reject) => {
+      let orderItems = await db
+        .get()
+        .collection(collection.ORDER_COLLECTION)
+        .aggregate([
+          {
+            $match: { _id: objectId(orderId) },
+          },
+          {
+            $unwind: "$products",
+          },
+          {
+            $project: {
+              item: "$products.item",
+              quantity: "$products.quantity",
+            },
+          },
+          {
+            $lookup: {
+              from: collection.PRODUCT_COLLECTION,
+              localField: "item",
+              foreignField: "_id",
+              as: "product",
+            },
+          },
+          {
+            $project: {
+              item: 1,
+              quantity: 1,
+              product: { $arrayElemAt: ["$product", 0] }
+            }
+          }
+        ])
+        .toArray();
+        resolve(orderItems);
+    })
   }
 };
--- routes/user.js
+++ routes/user.js
@@ -93,6 +93,25 @@ router.post('/remove-product', (req, res, next) => {
 })
 router.get('/place-order',verifyLogin, async (req, res) => {
   let total = await userHelpers.getTotalAmount(req.session.user._id)
-  res.render('user/place-order', {total})
+  res.render('user/place-order', {total, user: req.session.user})
+})
+router.post('/place-order', async (req, res) => {
+  console.log(req.body);
+  let products = await userHelpers.getCartProductList(req.body.userId);
+  let totalPrice = await userHelpers.getTotalAmount(req.body.userId);
+  userHelpers.placeOrder(req.body, products, totalPrice).then((response) => {
+    res.json({status:true})
+  })
+})
+router.get('/order-success', (req, res) => {
+  res.render('user/order-success', {user: req.session.user})
+})
+router.get('/orders', async (req, res) => {
+  let orders = await userHelpers.getUserOrders(req.session.user._id);
+  res.render('user/orders', {user: req.session.user, orders})
+})
+router.get('/view-order-products/:id', async (req, res) => {
+  let products = await userHelpers.getOrderProducts(req.params.id);
+  res.render('user/view-order-products', {user: req.session.user, products})
 })
 module.exports = router;
